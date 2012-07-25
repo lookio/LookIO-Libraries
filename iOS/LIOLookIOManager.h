8a805684cefc0ac026f380ac73324728fa8bf387
marc <marc@mc85472935.local>
2012-07-25 17:42:18 -0400

    Unstable version 171

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v170
+//  LookIO iOS Remote Support Client v171
 //  
 //  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"170"
+#define LOOKIO_VERSION_STRING @"171"
 
 @class LIOLookIOManager;
 
