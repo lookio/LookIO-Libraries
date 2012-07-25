04184aed4570066566dc6e4814d0bc79b586fbe7
Joe Toscano <jtoscano42@gmail.com>
2012-07-25 13:42:58 -0700

    Unstable Enterprise version 4

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,15 +1,15 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v152
+//  LookIO iOS Remote Support Client v4
 //  
-//  Copyright 2011-2012 LookIO, Inc. All rights reserved.
+//  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
 //  This header file is for use with LookIO.
 //  Documentation and support: http://support.look.io/
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"152"
+#define LOOKIO_VERSION_STRING @"4"
 
 @class LIOLookIOManager;
 
@@ -20,13 +20,13 @@
 - (void)lookIOManager:(LIOLookIOManager *)aManager didUpdateEnabledStatus:(BOOL)lookioIsEnabled;
 - (void)lookIOManagerDidHideControlButton:(LIOLookIOManager *)aManager;
 - (void)lookIOManagerDidShowControlButton:(LIOLookIOManager *)aManager;
+- (UIView *)lookIOManager:(LIOLookIOManager *)aManager linkViewForURL:(NSURL *)aURL;
 @end
 
 @interface LIOLookIOManager : NSObject
 
 @property(nonatomic, retain) UIImage *touchImage;
 @property(nonatomic, retain) NSString *targetAgentId;
-@property(nonatomic, assign) BOOL usesTLS;
 @property(nonatomic, retain) UIWindow *mainWindow;
 @property(nonatomic, readonly) BOOL enabled;
 @property(nonatomic, assign) id<LIOLookIOManagerDelegate> delegate;
@@ -34,6 +34,7 @@
 + (LIOLookIOManager *)sharedLookIOManager;
 - (void)performSetupWithDelegate:(id<LIOLookIOManagerDelegate>)aDelegate;
 - (void)recordCurrentUILocation:(NSString *)aLocationString;
+- (void)recordCurrentRequiredSkill:(NSString *)aRequiredSkill;
 - (void)beginSession;
 - (void)setSessionExtra:(id)anObject forKey:(NSString *)aKey;
 - (id)sessionExtraForKey:(NSString *)aKey;
