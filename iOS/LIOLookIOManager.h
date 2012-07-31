f2ffccb6a5eb62ea099fac8fde7c637ea6f16cba
Mac Build Server <marc@look.io>
2012-07-31 13:17:45 -0400

    Unstable version 177

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v176
+//  LookIO iOS Remote Support Client v177
 //  
 //  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"176"
+#define LOOKIO_VERSION_STRING @"177"
 
 @class LIOLookIOManager;
 
