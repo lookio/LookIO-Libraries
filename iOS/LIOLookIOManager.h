2db818c700760de7db2ff78fff69a3d70ad479b1
Mac Build Server <marc@look.io>
2012-10-17 14:47:40 -0400

    Release version 211

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,15 +1,15 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v152
+//  LookIO iOS Remote Support Client v211
 //  
-//  Copyright 2011-2012 LookIO, Inc. All rights reserved.
+//  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
 //  This header file is for use with LookIO.
 //  Documentation and support: http://support.look.io/
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"152"
+#define LOOKIO_VERSION_STRING @"211"
 
 @class LIOLookIOManager;
 
@@ -17,23 +17,26 @@
 @optional
 - (UIWindow *)lookIOManagerMainWindowForHostApp:(LIOLookIOManager *)aManager;
 - (BOOL)lookIOManager:(LIOLookIOManager *)aManager shouldRotateToInterfaceOrientation:(UIInterfaceOrientation)anOrientation;
+- (BOOL)lookIOManagerShouldAutorotate:(LIOLookIOManager *)aManager;
+- (NSInteger)lookIOManagerSupportedInterfaceOrientations:(LIOLookIOManager *)aManager;
 - (void)lookIOManager:(LIOLookIOManager *)aManager didUpdateEnabledStatus:(BOOL)lookioIsEnabled;
 - (void)lookIOManagerDidHideControlButton:(LIOLookIOManager *)aManager;
 - (void)lookIOManagerDidShowControlButton:(LIOLookIOManager *)aManager;
+- (id)lookIOManager:(LIOLookIOManager *)aManager linkViewForURL:(NSURL *)aURL;
 @end
 
 @interface LIOLookIOManager : NSObject
 
 @property(nonatomic, retain) UIImage *touchImage;
 @property(nonatomic, retain) NSString *targetAgentId;
-@property(nonatomic, assign) BOOL usesTLS;
 @property(nonatomic, retain) UIWindow *mainWindow;
 @property(nonatomic, readonly) BOOL enabled;
 @property(nonatomic, assign) id<LIOLookIOManagerDelegate> delegate;
 
 + (LIOLookIOManager *)sharedLookIOManager;
 - (void)performSetupWithDelegate:(id<LIOLookIOManagerDelegate>)aDelegate;
-- (void)recordCurrentUILocation:(NSString *)aLocationString;
+- (void)setUILocation:(NSString *)aLocationString;
+- (void)setSkill:(NSString *)aRequiredSkill;
 - (void)beginSession;
 - (void)setSessionExtra:(id)anObject forKey:(NSString *)aKey;
 - (id)sessionExtraForKey:(NSString *)aKey;
