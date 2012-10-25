eec75e4998f5a6468d84bd979c3f0efd53795a13
Mac Build Server <marc@look.io>
2012-10-25 14:36:23 -0400

    Release version 221

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v218
+//  LookIO iOS Remote Support Client v221
 //  
 //  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"218"
+#define LOOKIO_VERSION_STRING @"221"
 
 @class LIOLookIOManager;
 
