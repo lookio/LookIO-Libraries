bc8bbae03701ee246f4675d1d4322b91634b9f6e
Mac Build Server <marc@look.io>
2012-11-13 19:21:35 -0500

    Unstable version 232

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v231
+//  LookIO iOS Remote Support Client v232
 //  
 //  Copyright 2011-2012 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,38 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"231"
+#define LOOKIO_VERSION_STRING @"232"
+
+/*
+ * Plugin flow for creating and sending content:
+ * ---------------------------------------------
+ * 1) Host registers plugin (a class conforming to LIOPlugin) using registerPlugin:.
+ * 2) LookIO asks plugin for info necessary to add button(s) to attachment action sheet.
+ * 3) LookIO asks plugin for a UIViewController instance which is then presented modally.
+ * 4) Plugin tells LookIO that it's done/canceled via delegate. If done, a key string is
+ *    returned to LookIO.
+ * 5) LookIO asks plugin for a content thumbnail to be displayed in conversation flow.
+ * 6) If user taps bubble, LookIO asks plugin for a UIViewController instance which is
+ *    then presented modally in order to display the custom plugin content to the user.
+ * 7) Plugin tells LookIO via delegate when it's done displaying content.
+ */
+@protocol LIOPlugin;
+
+@protocol LIOPluginDelegate <NSObject>
+- (void)plugin:(id<LIOPlugin>)aPlugin didFinishChoosingContentWithResultKey:(NSString *)aKey;
+- (void)pluginDidCancelChoosingContent:(id<LIOPlugin>)aPlugin;
+- (void)pluginDidFinishDisplayingContent;
+@end
+
+@protocol LIOPlugin <NSObject>
+- (NSString *)pluginId; // Must be a unique string.
+- (NSArray *)pluginButtonLabels; // e.g. "Take Photo", "Choose Existing"
+- (UIViewController *)viewControllerForPluginButtonIndex:(NSInteger)anIndex;
+- (UIView *)thumbnailViewForPluginContentWithKey:(NSString *)aKey;
+- (UIViewController *)viewControllerForDisplayingPluginContentWithKey:(NSString *)aKey;
+- (void)resetPluginState;
+@property(nonatomic, assign) id<LIOPluginDelegate> pluginDelegate;
+@end
 
 @class LIOLookIOManager;
 
@@ -42,5 +73,6 @@
 - (id)sessionExtraForKey:(NSString *)aKey;
 - (void)addSessionExtras:(NSDictionary *)aDictionary;
 - (void)clearSessionExtras;
+- (BOOL)registerPlugin:(id<LIOPlugin>)aPlugin;
 
 @end
