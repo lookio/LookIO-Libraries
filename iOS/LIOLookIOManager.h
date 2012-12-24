1861b4164b48e8a3518f1f099b0acb4e277b25a5
Mac Build Server <marc@look.io>
2012-12-24 15:34:19 -0500

    Unstable version 248

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LivePerson iOS Remote Support Client v247
+//  LivePerson iOS Remote Support Client v248
 //  
 //  Copyright 2011-2013 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"247"
+#define LOOKIO_VERSION_STRING @"248"
 
 // Event constants.
 extern NSString *const kLPEventConversion;
