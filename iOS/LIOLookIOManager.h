1e3e58cbf01cd9f5ed282c4c378cec21423d3ae9
Mac Build Server <marc@look.io>
2013-06-11 17:05:19 -0400

    Pre surveys version 319

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LivePerson iOS Remote Support Client v318
+//  LivePerson iOS Remote Support Client v319
 //  
 //  Copyright 2011-2013 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"318"
+#define LOOKIO_VERSION_STRING @"319"
 
 // Event constants.
 // Use these with the "reportEvent" methods.
