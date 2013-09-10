12c7c4aacd491028608502b2c68dd62332f64f02
Mac Build Server <marc@look.io>
2013-09-10 06:50:51 -0400

    Unstable version 351

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LivePerson iOS Remote Support Client v350
+//  LivePerson iOS Remote Support Client v351
 //  
 //  Copyright 2011-2013 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"350"
+#define LOOKIO_VERSION_STRING @"351"
 
 // Event constants.
 // Use these with the "reportEvent" methods.
