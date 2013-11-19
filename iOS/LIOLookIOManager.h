27add21f1fd162803e7f45395315f59c615de635
marc <marc@mc055785547.local>
2013-11-19 16:39:23 -0500

    Unstable version 384

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LivePerson iOS Remote Support Client v381
+//  LivePerson iOS Remote Support Client v384
 //  
 //  Copyright 2011-2013 LivePerson, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"381"
+#define LOOKIO_VERSION_STRING @"384"
 
 // Event constants.
 // Use these with the "reportEvent" methods.
