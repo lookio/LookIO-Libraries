04cc947ad95e022989ea6f4ea7a56b2b03ed5e96
marc <marc@mc055785547.local>
2014-04-10 13:41:16 -0400

    Release version 420

--- iOS/LIOLookIOManager.h
+++ iOS/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //
 //  LIOLookIOManager.h
-//  LivePerson iOS Remote Support Client v414
+//  LivePerson iOS Remote Support Client v420
 //
 //  Copyright 2011-2014 LivePerson, Inc. All rights reserved.
 //
@@ -10,7 +10,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"414"
+#define LOOKIO_VERSION_STRING @"420"
 
 // Event constants.
 // Use these with the "reportEvent" methods.
