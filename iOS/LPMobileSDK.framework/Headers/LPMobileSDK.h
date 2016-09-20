b066c9bb7330e54bfe125e49303a217811af2e30
ci_user <ci_user@liveperson.com>
2016-09-20 22:06:12 +0300

    Pre-qa version 73

---
+++ iOS/LPMobileSDK.framework/Headers/LPMobileSDK.h
@@ -0,0 +1,18 @@
+//
+//  LPMobileSDK.h
+//  LPMobileSDK
+//
+//  Copyright 2011-2016 LivePerson, Inc. All rights reserved.
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPMobileSDK.
+FOUNDATION_EXPORT double LPMobileSDKVersionNumber;
+
+//! Project version string for LPMobileSDK.
+FOUNDATION_EXPORT const unsigned char LPMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPMobileSDK/PublicHeader.h>
+
+#import <LPMobileSDK/LIOLookIOManager.h>
